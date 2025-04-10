0a0a98ba949105832db2ad319e9b33989261ca3d
Jayfunf <simh3077@gmail.com>
2025-04-10 20:43:14 +0900

    init

---
+++ src/main/java/com/yoyakso/comket/ComketApplication.java
@@ -0,0 +1,14 @@
+package com.yoyakso.comket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComketApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Yoyakso!");
+		SpringApplication.run(ComketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoyakso/comket/ComketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoyakso.comket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
