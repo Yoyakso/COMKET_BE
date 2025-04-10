c1d101bb963a0240d34ad377b0372397dc02057b
Jayfunf <simh3077@gmail.com>
2025-04-10 20:51:58 +0900

    Delete: Test 코드 주석

--- src/test/java/com/yoyakso/comket/ComketApplicationTests.java
+++ src/test/java/com/yoyakso/comket/ComketApplicationTests.java
@@ -3,11 +3,11 @@ package com.yoyakso.comket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ComketApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ComketApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
