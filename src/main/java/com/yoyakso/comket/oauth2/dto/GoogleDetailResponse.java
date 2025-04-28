73c97f39d5f9d3ccf33dda057eea3c96f121db7e
Jayfunf <simh3077@gmail.com>
2025-04-29 00:09:14 +0900

    Update: Setter 추가

--- src/main/java/com/yoyakso/comket/oauth2/dto/GoogleDetailResponse.java
+++ src/main/java/com/yoyakso/comket/oauth2/dto/GoogleDetailResponse.java
@@ -5,10 +5,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class GoogleDetailResponse {
-    private String id;
-    private String email;
-    private String name;
-    private String picture;
+	private String id;
+	private String email;
+	private String name;
+	private String picture;
 }
--- src/main/java/com/yoyakso/comket/oauth2/dto/GoogleTokenResponse.java
+++ src/main/java/com/yoyakso/comket/oauth2/dto/GoogleTokenResponse.java
@@ -2,15 +2,16 @@ package com.yoyakso.comket.oauth2.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Service;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class GoogleTokenResponse {
-    private String access_token;
-    private String expires_in;
-    private String refresh_token;
-    private String scope;
-    private String token_type;
-    private String id_token;
+	private String access_token;
+	private String expires_in;
+	private String refresh_token;
+	private String scope;
+	private String token_type;
+	private String id_token;
 }
