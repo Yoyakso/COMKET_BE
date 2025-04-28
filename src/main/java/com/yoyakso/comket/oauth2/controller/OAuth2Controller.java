b7c3223043b500794895c2ec8285faf419ea4dae
Jayfunf <simh3077@gmail.com>
2025-04-28 22:36:12 +0900

    Update: 구글 로그인 API 추가 - 로그인 페이지 요청 API 및 구글 redirect API

--- src/main/java/com/yoyakso/comket/oauth2/controller/OAuth2Controller.java
+++ src/main/java/com/yoyakso/comket/oauth2/controller/OAuth2Controller.java
@@ -1,29 +1,28 @@
 package com.yoyakso.comket.oauth2.controller;
 
+import com.yoyakso.comket.oauth2.dto.GoogleDetailResponse;
+import com.yoyakso.comket.oauth2.dto.GoogleTokenResponse;
+import com.yoyakso.comket.oauth2.service.OAuth2Service;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/auth/oauth2")
-@CrossOrigin("*") // 테스트 환경
 public class OAuth2Controller {
+    private final OAuth2Service oAuth2Service;
 
-    @Value("${google_oauth2_client_id}")
-    private String googleClientId;
-    @Value("${google_oauth2_client_secret}")
-    private String googleClientSecret;
+    @Operation(method = "GET", description = "구글 로그인 페이지 URL 반환 메서드")
+    @GetMapping("/google")
+    public ResponseEntity<String> requestGoogleLogin() {
+        return ResponseEntity.ok(oAuth2Service.returnGoogleLoginPageUrl());
+    }
 
-    @Operation(method = "POST", description = "구글 로그인 요청 메서드")
-    @PostMapping("/google")
-    public String requestGoogleLogin() {
-        String requestURL = "https://accounts.google.com/o/oauth2/v2/auth?client_id=" + googleClientId
-                + "&redirect_uri=http://localhost:8080/api/v1/oauth2/google&response_type=code&scope=email%20profile%20openid&access_type=offline";
-        return requestURL;
+    @GetMapping("/google/login")
+    public ResponseEntity<GoogleDetailResponse> googleLoginCallback(@RequestParam String code) {
+        GoogleDetailResponse userInfo = oAuth2Service.getGoogleUserInfo(code);
+        return ResponseEntity.ok(userInfo);
     }
 }
