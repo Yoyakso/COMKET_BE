471433772cccf768f2daebc04800cbaa00b9556c
이태경 <tph00300@ajou.ac.kr>
2025-04-28 19:02:56 +0900

    ADD : DTO 적용 및 회원 수정, 탈퇴 API 작성

--- src/main/java/com/yoyakso/comket/member/MemberRepository.java
+++ src/main/java/com/yoyakso/comket/member/MemberRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	Member findByEmail(String email);
 
-	Member findByPhoneNumber(String phoneNumber);
-
-	Member findByName(String name);
-
-	Member findByIdAndName(Long id, String name);
+	boolean existsByEmail(String email);
+	boolean existsByNickname(String nickname);
 }
---
+++ src/main/java/com/yoyakso/comket/member/dto/MemberRegisterRequest.java
@@ -0,0 +1,16 @@
+package com.yoyakso.comket.member.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class MemberRegisterRequest {
+	private String email;
+	private String password;
+	private String nickname;
+	@JsonProperty("real_name")
+	private String realName;
+	// private int position;
+	// private String profileFileId;
+}
