6670fc1e4a6b19d45730a88fcec27c50025f0170
Jayfunf <simh3077@gmail.com>
2025-05-01 11:41:36 +0900

    Update: localhost:3333 접속 허용

--- src/main/java/com/yoyakso/comket/oauth2/controller/OAuth2Controller.java
+++ src/main/java/com/yoyakso/comket/oauth2/controller/OAuth2Controller.java
@@ -1,6 +1,7 @@
 package com.yoyakso.comket.oauth2.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/auth/oauth2")
+@CrossOrigin("http://localhost:3333")
 public class OAuth2Controller {
 	private final OAuth2Service oAuth2Service;
 
