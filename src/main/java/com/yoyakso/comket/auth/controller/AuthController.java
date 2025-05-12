be20ff0bc1b47b687c37b0479d1b6e856a7374a1
Jayfunf <simh3077@gmail.com>
2025-05-13 03:18:34 +0900

    Add: Token 재발급 API 추가

--- src/main/java/com/yoyakso/comket/auth/controller/AuthController.java
+++ src/main/java/com/yoyakso/comket/auth/controller/AuthController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yoyakso.comket.auth.dto.LoginRequest;
 import com.yoyakso.comket.auth.dto.LoginResponse;
+import com.yoyakso.comket.auth.dto.TokenReissueRequest;
+import com.yoyakso.comket.auth.dto.TokenReissueResponse;
 import com.yoyakso.comket.auth.service.AuthService;
+import com.yoyakso.comket.exception.CustomException;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -37,6 +41,23 @@ public class AuthController {
 	@PostMapping("/login")
 	public ResponseEntity<LoginResponse> requestLogin(@RequestBody LoginRequest requestInfo) {
 		LoginResponse response = authService.login(requestInfo);
+		return ResponseEntity.ok(response);
+	}
+
+	@Operation(method = "POST", description = "만료 토큰 재발급 API")
+	@PostMapping("/reissue")
+	public ResponseEntity<TokenReissueResponse> reissueToken(
+		@RequestBody TokenReissueRequest request,
+		@RequestHeader("Authorization") String accessHeader
+	) {
+
+		if (accessHeader == null || !accessHeader.startsWith("Bearer ")) {
+			throw new CustomException("TOKEN_NOT_FOUND", "AccessToken이 없습니다.");
+		}
+
+		String accessToken = accessHeader.substring(7);
+		TokenReissueResponse response = authService.reissueToken(accessToken, request.getRefreshToken());
+
 		return ResponseEntity.ok(response);
 	}
 }
