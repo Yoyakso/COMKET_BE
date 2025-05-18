b1723481e23833eb0916e0f39d5b7e2c2e45e597
Jayfunf <simh3077@gmail.com>
2025-05-18 20:26:44 +0900

    Update: ec2 실행 시 자동 실행 코드 추가

--- install-kafka.sh
+++ install-kafka.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker run -d --name zookeeper -p 2181:2181 \
+docker run -d --name zookeeper --restart unless-stopped -p 2181:2181 \
   -e ALLOW_ANONYMOUS_LOGIN=yes \
   bitnami/zookeeper:3.7
 
 sleep 3
 
-docker run -d --name kafka -p 9092:9092 \
+docker run -d --name kafka --restart unless-stopped -p 9092:9092 \
   -e KAFKA_BROKER_ID=1 \
   -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181 \
   -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092 \
