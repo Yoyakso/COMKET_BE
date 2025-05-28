1f4757608478ffdd271c4612dc14c0b01e0613f5
Jayfunf <simh3077@gmail.com>
2025-05-28 13:48:45 +0900

    Add: dto 추가

--- src/main/java/com/yoyakso/comket/ai/dto/AiSummaryWithActionItemsResponse.java
+++ src/main/java/com/yoyakso/comket/ai/dto/AiSummaryWithActionItemsResponse.java
@@ -1,10 +1,5 @@
 package com.yoyakso.comket.ai.dto;
 
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.yoyakso.comket.ticket.enums.TicketPriority;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,10 +9,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AiSummaryWithActionItemsResponse {
 	private String summary;
-	private String title;
-	private TicketPriority priority;
-	private ActionItemAssigneeDto memberInfo;
-
-	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-	private LocalDateTime sentAt;
+	private ActionItemContentDto[] actionItems;
 }
