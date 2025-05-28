429bcad7997392f3589912342dc77561f5fb939b
Jayfunf <simh3077@gmail.com>
2025-05-28 13:51:48 +0900

    Update: ai 기능 연결

--- src/main/java/com/yoyakso/comket/ai/controller/AiController.java
+++ src/main/java/com/yoyakso/comket/ai/controller/AiController.java
@@ -4,11 +4,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.yoyakso.comket.ai.dto.AiEyeLevelSummaryResponse;
 import com.yoyakso.comket.ai.dto.AiSummaryWithActionItemsResponse;
+import com.yoyakso.comket.ai.service.AiService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,20 +15,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/")
 public class AiController {
+	private final AiService aiService;
 
 	@GetMapping("{ticketId}/ai")
 	public ResponseEntity<AiSummaryWithActionItemsResponse> getSummaryAndActionItems(
 		@PathVariable("ticketId") Long ticketId
 	) {
-		
+		AiSummaryWithActionItemsResponse response = aiService.summarizeThread(ticketId);
+		return ResponseEntity.ok(response);
 	}
 
-	@GetMapping("{ticketId}/ai")
-	public ResponseEntity<AiEyeLevelSummaryResponse> getEyeLevelSummary(
-		@PathVariable("ticketId") Long ticketId,
-		@RequestParam("responsibility") String responsibility
-	) {
-
-	}
+	// @GetMapping("{ticketId}/ai")
+	// public ResponseEntity<AiEyeLevelSummaryResponse> getEyeLevelSummary(
+	// 	@PathVariable("ticketId") Long ticketId,
+	// 	@RequestParam("responsibility") String responsibility
+	// ) {
+	//
+	// }
 
 }
