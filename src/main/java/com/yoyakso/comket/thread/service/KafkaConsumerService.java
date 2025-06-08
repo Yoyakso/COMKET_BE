63ec34e71d80082b6687b51368d5ff31366f11cb
Jayfunf <simh3077@gmail.com>
2025-06-08 19:11:55 +0900

    Update: 메시지 타입에 따라 DB 저장 / 저스트 브로드캐스트

--- src/main/java/com/yoyakso/comket/thread/service/KafkaConsumerService.java
+++ src/main/java/com/yoyakso/comket/thread/service/KafkaConsumerService.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yoyakso.comket.thread.dto.ThreadMessageDto;
+import com.yoyakso.comket.thread.entity.ThreadMessage;
 import com.yoyakso.comket.thread.util.ThreadSocketHandler;
 
 import lombok.RequiredArgsConstructor;
@@ -22,9 +24,29 @@ public class KafkaConsumerService { // Kafka에서는
 
 	public void handleMessage(String jsonMessage, Long ticketId) {
 		try {
-			ThreadMessageDto messageDto = objectMapper.readValue(jsonMessage, ThreadMessageDto.class);
-			threadMessageService.saveAsync(messageDto);
-			threadSocketHandler.sendToTicket(ticketId, messageDto);
+			JsonNode rootNode = objectMapper.readTree(jsonMessage);
+			String type = rootNode.get("type").asText(); // "message_created" or "message_updated"
+			JsonNode dataNode = rootNode.get("data");
+
+			if ("message_created".equals(type)) {
+				ThreadMessageDto messageDto = objectMapper.treeToValue(dataNode, ThreadMessageDto.class);
+				ThreadMessage savedEntity = threadMessageService.saveAsync(messageDto);
+				ThreadMessageDto updatedDto = ThreadMessageDto.builder()
+					.threadId(savedEntity.getId())   // 저장된 id로 세팅
+					.ticketId(savedEntity.getTicketId())
+					.senderMemberId(savedEntity.getSenderMemberId())
+					.senderName(messageDto.getSenderName()) // 이름은 요청에서
+					.content(savedEntity.getContent())
+					.sentAt(savedEntity.getSentAt())
+					.isModified(savedEntity.getIsModified())
+					.build();
+				threadSocketHandler.sendToTicket(ticketId, updatedDto); // WebSocket broadcast
+			} else if ("message_updated".equals(type)) {
+				ThreadMessageDto updatedMessage = objectMapper.treeToValue(dataNode, ThreadMessageDto.class);
+				threadSocketHandler.sendToTicket(ticketId, updatedMessage); // DB 저장 없이 broadcast만
+			} else { // Delete 등의 상태도 추가
+				System.out.println("@@@ 알 수 없는 메시지 타입 수신: " + type);
+			}
 		} catch (Exception e) {
 			// TODO: 로그 추가
 			System.out.println("@@@ Kafka 메시지 처리 실패" + e.getMessage());
