c8cb1157de135aa352991997332291a71da90570
Jayfunf <simh3077@gmail.com>
2025-06-09 02:51:50 +0900

    Update: 리소스 필드 추가 및 dto 수정

--- src/main/java/com/yoyakso/comket/thread/dto/ThreadMessageReplyRequestDto.java
+++ src/main/java/com/yoyakso/comket/thread/dto/ThreadMessageReplyRequestDto.java
@@ -1,9 +1,11 @@
 package com.yoyakso.comket.thread.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +21,9 @@ public class ThreadMessageReplyRequestDto {
 	private Long senderMemberId;
 	private String senderName;
 	private String reply;
+
+	@Nullable
+	private List<String> resources;
 
 	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	private LocalDateTime sentAt;
